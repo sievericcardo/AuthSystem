27442305d667ea36e1febcb521ac1a52cdc98233
Riccardo Sieve <sievericcardo@gmail.com>
2023-12-08 06:47:39 +0100

    Update AppConfig.kt
    removed comment

--- Auth/src/main/kotlin/no/uio/auth/config/AppConfig.kt
+++ Auth/src/main/kotlin/no/uio/auth/config/AppConfig.kt
@@ -6,5 +6,4 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = arrayOf("no.uio.auth"))
 public class AppConfig {
-    // Additional configuration if needed
 }
