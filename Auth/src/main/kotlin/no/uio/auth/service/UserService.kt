36c5cfffc0f8dcbff85b20cf1bbd504a11b639b9
Riccardo Sieve <sievericcardo@gmail.com>
2023-12-08 06:13:41 +0100

    Create UserService.kt
    Added base service for user

---
+++ Auth/src/main/kotlin/no/uio/auth/service/UserService.kt
@@ -0,0 +1,26 @@
+package no.uio.smol.auth.no.uio.auth.service
+
+import no.uio.smol.auth.no.uio.auth.model.User
+import no.uio.smol.auth.no.uio.auth.repository.UserRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import java.util.*
+
+
+@Service
+class UserService @Autowired constructor(private val userRepository: UserRepository) {
+    val allUsers: List<Any?>
+        get() = userRepository.findAll()
+
+    fun getUserById(userId: Long): Optional<User?> {
+        return userRepository.findById(userId)
+    }
+
+    fun createUser(user: User): User {
+        return userRepository.save(user)
+    }
+
+    fun deleteUser(userId: Long) {
+        userRepository.deleteById(userId)
+    }
+}
