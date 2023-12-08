8de7a5f2d21f5c8ec7526cf16fcd183ebbbf04e3
Riccardo Sieve <sievericcardo@gmail.com>
2023-12-08 17:22:26 +0100

    Update AppConfig.kt
    changed AppConfig to open class

--- Auth/src/main/kotlin/no/uio/auth/config/AppConfig.kt
+++ Auth/src/main/kotlin/no/uio/auth/config/AppConfig.kt
@@ -1,9 +1,9 @@
-package no.uio.smol.auth.no.uio.auth.config
+package no.uio.auth.config
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
 
 @Configuration
 @ComponentScan(basePackages = arrayOf("no.uio.auth"))
-public class AppConfig {
+open class AppConfig {
 }
