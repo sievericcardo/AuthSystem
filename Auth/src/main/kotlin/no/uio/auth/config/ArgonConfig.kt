134a621827c1e15c8926580239393455f1ef4ece
Riccardo Sieve <sievericcardo@gmail.com>
2023-12-08 17:22:36 +0100

    Update ArgonConfig.kt
    set ArgonConfig to openclass

--- Auth/src/main/kotlin/no/uio/auth/config/ArgonConfig.kt
+++ Auth/src/main/kotlin/no/uio/auth/config/ArgonConfig.kt
@@ -1,13 +1,11 @@
-package no.uio.smol.auth.no.uio.auth.config
+package no.uio.auth.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-//import org.springframework.boot.context.properties.ConstructorBinding
-//import org.springframework.boot.context.properties.bind.ConstructorBinding
+import org.springframework.boot.context.properties.bind.ConstructorBinding
 import org.springframework.context.annotation.Configuration
 
-@Configuration
 @ConfigurationProperties(prefix = "argon2")
-data class ArgonConfig(
+open class ArgonConfig(
     val saltLength: Int,
     val hashLength: Int,
     val parallelism: Int,
