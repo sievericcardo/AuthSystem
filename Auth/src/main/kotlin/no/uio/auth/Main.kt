50860cb36a491c4373189f985e043945d843bd28
Riccardo Sieve <sievericcardo@gmail.com>
2023-12-08 17:21:49 +0100

    Moved Main

---
+++ Auth/src/main/kotlin/no/uio/auth/Main.kt
@@ -0,0 +1,21 @@
+package no.uio.auth
+
+import no.uio.auth.config.ArgonConfig
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
+@SpringBootApplication
+@EnableConfigurationProperties(ArgonConfig::class)
+//@EntityScan("no.uio.auth.model.User")
+@EnableJpaRepositories("no.*")
+@ComponentScan(basePackages = arrayOf("no.*"))
+@EntityScan("no.*")
+open class Auth
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Auth::class.java, *args)
+}
