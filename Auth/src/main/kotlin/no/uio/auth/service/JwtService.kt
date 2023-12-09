e16e72c879a64a6cc090500e3c1e085729d8dbd3
Riccardo Sieve <sievericcardo@gmail.com>
2023-12-09 22:41:21 +0100

    Create JwtService.kt
    Added JwtService class

---
+++ Auth/src/main/kotlin/no/uio/auth/service/JwtService.kt
@@ -0,0 +1,62 @@
+package no.uio.auth.service
+
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.stereotype.Component
+import java.util.*
+import javax.crypto.SecretKey
+
+
+@Component
+class JwtService {
+    var key: SecretKey = Jwts.SIG.HS256.key().build();
+
+    private fun extractUsername(token: String?): String {
+        return extractClaim(token) { it.subject }
+    }
+
+    private fun extractExpiration(token: String?): Date {
+        return extractClaim(token) { it.expiration }
+    }
+
+    private fun <T> extractClaim(token: String?, claimsResolver: (Claims) -> T): T {
+        val claims = extractAllClaims(token)
+        return claimsResolver(claims)
+    }
+
+    private fun extractAllClaims(token: String?): Claims {
+        return Jwts
+            .parser()
+            .verifyWith(key)
+            .build()
+            .parseSignedClaims(token)
+            .getPayload()
+    }
+
+    private fun isTokenExpired(token: String?): Boolean {
+        return extractExpiration(token).before(Date())
+    }
+
+    fun validateToken(token: String?, userDetails: UserDetails): Boolean {
+        val username = extractUsername(token)
+        return (username == userDetails.username && !isTokenExpired(token))
+    }
+
+
+    fun GenerateToken(username: String): String {
+        val claims: Map<String, Any?> = HashMap()
+        return createToken(claims, username)
+    }
+
+
+    private fun createToken(claims: Map<String, Any?>, username: String): String {
+        return Jwts.builder()
+            .claims(claims)
+            .subject(username)
+            .issuedAt(Date(System.currentTimeMillis()))
+            .expiration(Date(System.currentTimeMillis() + 1000 * 60 * 1))
+            .signWith(key)
+            .compact()
+    }
+}
