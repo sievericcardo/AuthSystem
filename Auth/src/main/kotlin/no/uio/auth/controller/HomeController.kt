918aea4e02256d3c62cefe54131375ce405acd17
Riccardo Sieve <sievericcardo@gmail.com>
2023-12-09 17:00:14 +0100

    Added Swagger for documentation

--- Auth/src/main/kotlin/no/uio/auth/controller/HomeController.kt
+++ Auth/src/main/kotlin/no/uio/auth/controller/HomeController.kt
@@ -9,17 +9,38 @@ import org.springframework.http.ResponseEntity
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder
 import no.uio.auth.service.UserService
 import no.uio.auth.model.User
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiParam
+import io.swagger.annotations.ApiResponse
+import io.swagger.annotations.ApiResponses
 
 data class UserRequest(val username: String, val email: String, val password: String)
 data class SignUpRequest(val userRequest: UserRequest, val passwordCheck: String)
 data class SignInRequest(val userRequest: UserRequest)
 
+/**
+ * The home controller handles the signup and signin endpoints.
+ */
 @RestController
 @RequestMapping("/api")
 class HomeController(private val argonConfig: ArgonConfig, private val userService: UserService) {
 
+    /**
+     * Sign up a new user.
+     *
+     * @param signUpRequest The request to sign up a new user.
+     */
+    @ApiOperation(value = "Sign up a new user")
+    @ApiResponses(value = [
+        ApiResponse(code = 200, message = "Successfully created user"),
+        ApiResponse(code = 401, message = "You are not authorized to create a user"),
+        ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+        ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
+        ApiResponse(code = 400, message = "Passwords do not match"),
+        ApiResponse(code = 500, message = "Internal server error")
+    ])
     @PostMapping("/signup")
-    fun signUp(@RequestBody signUpRequest: SignUpRequest): ResponseEntity<String> {
+    fun signUp(@ApiParam(value = "Request to sign up a new user") @RequestBody signUpRequest: SignUpRequest): ResponseEntity<String> {
         if (signUpRequest.userRequest.password != signUpRequest.passwordCheck) {
             return ResponseEntity.badRequest().body("Passwords do not match")
         }
@@ -39,8 +60,22 @@ class HomeController(private val argonConfig: ArgonConfig, private val userServi
         return ResponseEntity.ok("User created successfully")
     }
 
+    /**
+     * Sign in a user.
+     *
+     * @param signInRequest The request to sign in a user.
+     */
+    @ApiOperation(value = "Sign in a user")
+    @ApiResponses(value = [
+        ApiResponse(code = 200, message = "Successfully signed in user"),
+        ApiResponse(code = 401, message = "You are not authorized to sign in a user"),
+        ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+        ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
+        ApiResponse(code = 400, message = "Invalid username and/or password"),
+        ApiResponse(code = 500, message = "Internal server error")
+    ])
     @PostMapping("/signin")
-    fun signIn(@RequestBody signInRequest: SignInRequest): ResponseEntity<String> {
+    fun signIn(@ApiParam(value = "Request to sign in a new user") @RequestBody signInRequest: SignInRequest): ResponseEntity<String> {
         val user = userService.findByUsername(signInRequest.userRequest.username)
             ?: return ResponseEntity.badRequest().body("User not found")
 
