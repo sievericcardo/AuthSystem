51b02b6a15110c203160352437f7c41988b0865f
Riccardo Sieve <sievericcardo@gmail.com>
2023-12-09 22:40:54 +0100

    Update SwaggerConfig.kt
    Commented (should be removed)

--- Auth/src/main/kotlin/no/uio/auth/config/SwaggerConfig.kt
+++ Auth/src/main/kotlin/no/uio/auth/config/SwaggerConfig.kt
@@ -1,22 +1,20 @@
-package no.uio.auth.config
-
-import springfox.documentation.builders.PathSelectors
-import springfox.documentation.builders.RequestHandlerSelectors
-import springfox.documentation.spi.DocumentationType
-import springfox.documentation.spring.web.plugins.Docket
-import springfox.documentation.swagger2.annotations.EnableSwagger2
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-@EnableSwagger2
-open class SwaggerConfig {
-    @Bean
-    open fun api(): Docket {
-        return Docket(DocumentationType.SWAGGER_2)
-            .select()
-            .apis(RequestHandlerSelectors.basePackage("no.uio.auth.controller"))
-            .paths(PathSelectors.any())
-            .build()
-    }
-}
+//package no.uio.auth.config
+//
+//import springfox.documentation.builders.PathSelectors
+//import springfox.documentation.builders.RequestHandlerSelectors
+//import springfox.documentation.spi.DocumentationType
+//import springfox.documentation.spring.web.plugins.Docket
+//import org.springframework.context.annotation.Bean
+//import org.springframework.context.annotation.Configuration
+//
+//@Configuration
+//open class SwaggerConfig {
+//    @Bean
+//    open fun api(): Docket {
+//        return Docket(DocumentationType.SWAGGER_2)
+//            .select()
+//            .apis(RequestHandlerSelectors.basePackage("no.uio.auth.controller"))
+//            .paths(PathSelectors.any())
+//            .build()
+//    }
+//}
