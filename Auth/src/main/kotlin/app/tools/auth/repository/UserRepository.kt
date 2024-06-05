af0c733a541f60299c4a371ae6c8325697826884
Riccardo Sieve <sievericcardo@gmail.com>
2024-06-05 17:24:43 +0200

    Update UserRepository.kt
    Modified Id from Long to String

--- Auth/src/main/kotlin/app/tools/auth/repository/UserRepository.kt
+++ Auth/src/main/kotlin/app/tools/auth/repository/UserRepository.kt
@@ -3,7 +3,7 @@ package app.tools.auth.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import app.tools.auth.model.User
 
-interface UserRepository : JpaRepository<User?, Long?> {
+interface UserRepository : JpaRepository<User?, String?> {
     fun findByUsername(username: String?): User?
     fun findByEmail(email: String?): User?
 }
