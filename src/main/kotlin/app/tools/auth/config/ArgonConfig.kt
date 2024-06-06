36fa76153459b0452ed8401bbae53092cdcecd1c
Riccardo Sieve <sievericcardo@gmail.com>
2024-06-06 14:45:19 +0200

    Update ArgonConfig.kt
    changed class to use @Value

--- src/main/kotlin/app/tools/auth/config/ArgonConfig.kt
+++ src/main/kotlin/app/tools/auth/config/ArgonConfig.kt
@@ -1,14 +1,27 @@
 package app.tools.auth.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.bind.ConstructorBinding
 import org.springframework.context.annotation.Configuration
 
+@Configuration
 @ConfigurationProperties(prefix = "argon2")
-open class ArgonConfig(
-    val saltLength: Int,
-    val hashLength: Int,
-    val parallelism: Int,
-    val memory: Int,
-    val iterations: Int
-)
+open class ArgonConfig {
+    @Value("\${argon2.saltLength}")
+    var saltLength: Int = 0
+
+    @Value("\${argon2.hashLength}")
+    var hashLength: Int = 0
+
+    @Value("\${argon2.parallelism}")
+    var parallelism: Int = 0
+
+    @Value("\${argon2.memory}")
+    var memory: Int = 0
+
+    @Value("\${argon2.iterations}")
+    var iterations: Int = 0
+
+    @Value("\${argon2.encoding}")
+    var encoding: String = ""
+}
